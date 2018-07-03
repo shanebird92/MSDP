b34426ce2bf50c96ac29392559c98d19dd34df7b
student <peng@csstudent07.ucd.ie>
2018-07-03 09:10:07 +0100

    2018.07.03 update ann.py from peng

--- backup/peng/scripts/ann.py
+++ backup/peng/scripts/ann.py
@@ -27,20 +27,32 @@ def prediction(line, startID, endID):
     sequence_a = 0
     sequence_b = 0
     if startID in stops_a and endID in stops_a:
-        sequence_a = stops_a.index(endID) - stops_a.index(startID)
+        stop_location = stops_a.index(endID)
+        start_location = stops_a.index(startID)
+        sequence_a = stop_location - start_location
     if startID in stops_b and endID in stops_b:
-        sequence_b = stops_b.index(endID) - stops_b.index(startID)
+        stop_location = stops_b.index(endID)
+        start_location = stops_b.index(startID)
+        sequence_b = stop_location - start_location
 
     if sequence_a > sequence_b:
         pairs = sequence_a
         model_file = model_file_a
         station_number = len(stops_a)
         inputFeatures = inputFeatures_a
+        plannedTime_first = df_a[df_a.columns[0]].iloc[1]
+        plannedTime_end = df_a[df_a.columns[-1]].iloc[1]
+        plannedTime_start = df_a[df_a.columns[start_location]].iloc[1]
+        plannedTime_stop = df_a[df_a.columns[stop_location]].iloc[1]
     elif sequence_b > sequence_a:
         pairs = sequence_b
         model_file = model_file_b
         station_number = len(stops_b)
         inputFeatures = inputFeatures_b
+        plannedTime_first = df_b[df_b.columns[0]].iloc[1]
+        plannedTime_end = df_b[df_b.columns[-1]].iloc[1]
+        plannedTime_start = df_b[df_b.columns[start_location]].iloc[1]
+        plannedTime_stop = df_b[df_b.columns[stop_location]].iloc[1]
     else:
         return 0
 
@@ -48,13 +60,22 @@ def prediction(line, startID, endID):
     new_clf = pickle.load(pkl_file)
     predictions = new_clf.predict([inputFeatures])
     
-    pair_time = (predictions[0][1]-predictions[0][0])/(station_number-1)
+    pred_full_time = predictions[0][1] - predictions[0][0]
+    planned_full_time = plannedTime_end - plannedTime_first
+    planned_pairs_time = plannedTime_stop - plannedTime_start
+    #pair_time = (predictions[0][1]-predictions[0][0])/(station_number-1)
+    #print("end/first: {}/{}".format(predictions[0][1],predictions[0][0]))
+    #print("stop/start: {}/{}".format(plannedTime_stop, plannedTime_start))
     #print(pair_time, pairs)
-    return pair_time * pairs
+    #return pair_time * pairs
+    # planned_full_time/planned_pairs_time = pred_full_time/pred_pairs_time
+    pred_pairs_time = pred_full_time * planned_pairs_time / planned_full_time
+    return pred_pairs_time
 
 def main():
     print(prediction('39A', 1913, 1660))
     print(prediction('39A', 1864, 335))
+    print(prediction('39A', 6112, 1867))
 
 if __name__ == '__main__':
     main()
