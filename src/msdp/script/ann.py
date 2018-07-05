e03ebc608e98fc6e779c8ecd213c622b27919b7b
student <peng@csstudent07.ucd.ie>
2018-07-05 13:10:01 +0100

    2018.07.05 update ann.py to support multiple Lines choosing from peng

--- src/msdp/script/ann.py
+++ src/msdp/script/ann.py
@@ -1,10 +1,24 @@
-import pickle
-import csv
+import pickle, os
+import csv, json
 import pandas as pd
 import numpy as np
 
-def prediction(line, startID, endID, targetTime):
-    '''
+class Ann:
+    def __init__(self, startID, endID, targetTime, DEBUG=False):
+        self.__data_path = os.path.dirname(os.path.abspath(__file__)) + "/../../../data"
+        self.__startid = startID
+        self.__endid = endID
+        self.__targettime = targetTime
+        self.__debug = DEBUG
+
+    @staticmethod
+    def get_mean_timetable(df, start_location, target_time):
+        station_column = '{}_P'.format(str(start_location).zfill(3))
+        mean_array = df[abs(df[station_column]-target_time) == min(abs(df[station_column]-target_time))].mean()
+        return mean_array.values
+
+    def prediction(self, line):
+        '''
 	- input value:
 		line: bus route, such as 39A, 46A, etc.
 		startID: start station ID
@@ -15,103 +29,134 @@ def prediction(line, startID, endID, targetTime):
         - Return Value:
 		The seconds taken from start station to end station,
                 based on ANN prediction
-    '''
-    model_file_a = "{}_a_2017_06.clf".format(line)
-    model_file_b = "{}_b_2017_06.clf".format(line)
+        '''
+        startID = self.__startid
+        endID = self.__endid
+        targetTime = self.__targettime
+        try:
+            model_file_a = self.__data_path + "/{}_a_2017_06.clf".format(line)
+            model_file_b = self.__data_path + "/{}_b_2017_06.clf".format(line)
 
-    time_table_file_a = "{}_a_timeTable.csv".format(line) 
-    time_table_file_b = "{}_b_timeTable.csv".format(line) 
+            time_table_file_a = self.__data_path + "/{}_a_timeTable.csv".format(line) 
+            time_table_file_b = self.__data_path + "/{}_b_timeTable.csv".format(line) 
 
-    df_a = pd.read_csv(time_table_file_a, index_col=0)
-    df_b = pd.read_csv(time_table_file_b, index_col=0)
+            df_a = pd.read_csv(time_table_file_a, index_col=0)
+            df_b = pd.read_csv(time_table_file_b, index_col=0)
+
+        except Exception as e:
+            if self.__debug:
+                print("Warning: Line {} {}".format(line, str(e)))
+            return 0.0
+
+        stops_a = []
+        inputFeatures_a = []
+        for column in df_a.columns:
+            stops_a.append(df_a[column].loc[0])
+            inputFeatures_a.append(df_a[column].iloc[1])
+        stops_b = []
+        inputFeatures_b = []
+        for column in df_b.columns:
+            stops_b.append(df_b[column].loc[0])
+            inputFeatures_b.append(df_b[column].iloc[1])
+
+        sequence_a = 0
+        sequence_b = 0
+        if startID in stops_a and endID in stops_a:
+            stop_location = stops_a.index(endID)
+            start_location = stops_a.index(startID)
+            sequence_a = stop_location - start_location
+        if startID in stops_b and endID in stops_b:
+            stop_location = stops_b.index(endID)
+            start_location = stops_b.index(startID)
+            sequence_b = stop_location - start_location
 
 
-    stops_a = []
-    inputFeatures_a = []
-    for column in df_a.columns:
-        stops_a.append(df_a[column].loc[0])
-        inputFeatures_a.append(df_a[column].iloc[1])
-    stops_b = []
-    inputFeatures_b = []
-    for column in df_b.columns:
-        stops_b.append(df_b[column].loc[0])
-        inputFeatures_b.append(df_b[column].iloc[1])
+        if sequence_a > sequence_b:
+            pairs = sequence_a
+            model_file = model_file_a
+            station_number = len(stops_a)
+            inputFeatures = inputFeatures_a
+            plannedTimeArray = self.get_mean_timetable(df_a, start_location, targetTime)
 
-    sequence_a = 0
-    sequence_b = 0
-    if startID in stops_a and endID in stops_a:
-        stop_location = stops_a.index(endID)
-        start_location = stops_a.index(startID)
-        sequence_a = stop_location - start_location
-    if startID in stops_b and endID in stops_b:
-        stop_location = stops_b.index(endID)
-        start_location = stops_b.index(startID)
-        sequence_b = stop_location - start_location
+            if len(plannedTimeArray) == station_number:
+                plannedTime_first = plannedTimeArray[0]
+                plannedTime_end = plannedTimeArray[-1]
+                plannedTime_start = plannedTimeArray[start_location]
+                plannedTime_stop = plannedTimeArray[stop_location]
+            else:
+                print("Can not get mean planned arrival time from time table!")
+                plannedTime_first = df_a[df_a.columns[0]].iloc[1]
+                plannedTime_end = df_a[df_a.columns[-1]].iloc[1]
+                plannedTime_start = df_a[df_a.columns[start_location]].iloc[1]
+                plannedTime_stop = df_a[df_a.columns[stop_location]].iloc[1]
+        elif sequence_b > sequence_a:
+            pairs = sequence_b
+            model_file = model_file_b
+            station_number = len(stops_b)
+            inputFeatures = inputFeatures_b
+            plannedTimeArray = self.get_mean_timetable(df_b, start_location, targetTime)
 
-    def get_mean_timetable(df, start_location, target_time):
-        station_column = '{}_P'.format(str(start_location).zfill(3))
-        mean_array = df[abs(df[station_column]-target_time) == min(abs(df[station_column]-target_time))].mean()
-        return mean_array.values
-
-    if sequence_a > sequence_b:
-        pairs = sequence_a
-        model_file = model_file_a
-        station_number = len(stops_a)
-        inputFeatures = inputFeatures_a
-        plannedTimeArray = get_mean_timetable(df_a, start_location, targetTime)
-
-        if len(plannedTimeArray) == station_number:
-            plannedTime_first = plannedTimeArray[0]
-            plannedTime_end = plannedTimeArray[-1]
-            plannedTime_start = plannedTimeArray[start_location]
-            plannedTime_stop = plannedTimeArray[stop_location]
+            if len(plannedTimeArray) == station_number:
+                plannedTime_first = plannedTimeArray[0]
+                plannedTime_end = plannedTimeArray[-1]
+                plannedTime_start = plannedTimeArray[start_location]
+                plannedTime_stop = plannedTimeArray[stop_location]
+            else:
+                print("Can not get mean planned arrival time from time table!")
+                plannedTime_first = df_b[df_b.columns[0]].iloc[1]
+                plannedTime_end = df_b[df_b.columns[-1]].iloc[1]
+                plannedTime_start = df_b[df_b.columns[start_location]].iloc[1]
+                plannedTime_stop = df_b[df_b.columns[stop_location]].iloc[1]
         else:
-            print("Can not get mean planned arrival time from time table!")
-            plannedTime_first = df_a[df_a.columns[0]].iloc[1]
-            plannedTime_end = df_a[df_a.columns[-1]].iloc[1]
-            plannedTime_start = df_a[df_a.columns[start_location]].iloc[1]
-            plannedTime_stop = df_a[df_a.columns[stop_location]].iloc[1]
-    elif sequence_b > sequence_a:
-        pairs = sequence_b
-        model_file = model_file_b
-        station_number = len(stops_b)
-        inputFeatures = inputFeatures_b
-        plannedTimeArray = get_mean_timetable(df_b, start_location, targetTime)
+            return 0
 
-        if len(plannedTimeArray) == station_number:
-            plannedTime_first = plannedTimeArray[0]
-            plannedTime_end = plannedTimeArray[-1]
-            plannedTime_start = plannedTimeArray[start_location]
-            plannedTime_stop = plannedTimeArray[stop_location]
-        else:
-            print("Can not get mean planned arrival time from time table!")
-            plannedTime_first = df_b[df_b.columns[0]].iloc[1]
-            plannedTime_end = df_b[df_b.columns[-1]].iloc[1]
-            plannedTime_start = df_b[df_b.columns[start_location]].iloc[1]
-            plannedTime_stop = df_b[df_b.columns[stop_location]].iloc[1]
-    else:
-        return 0
-
-    pkl_file = open(model_file, 'rb')
-    new_clf = pickle.load(pkl_file)
-    predictions = new_clf.predict([inputFeatures])
+        pkl_file = open(model_file, 'rb')
+        new_clf = pickle.load(pkl_file)
+        predictions = new_clf.predict([inputFeatures])
     
-    pred_full_time = predictions[0][1] - predictions[0][0]
-    planned_full_time = plannedTime_end - plannedTime_first
-    planned_pairs_time = plannedTime_stop - plannedTime_start
-    #pair_time = (predictions[0][1]-predictions[0][0])/(station_number-1)
-    #print("end/first: {}/{}".format(predictions[0][1],predictions[0][0]))
-    #print("stop/start: {}/{}".format(plannedTime_stop, plannedTime_start))
-    #print(pair_time, pairs)
-    #return pair_time * pairs
-    # planned_full_time/planned_pairs_time = pred_full_time/pred_pairs_time
-    pred_pairs_time = pred_full_time * planned_pairs_time / planned_full_time
-    return pred_pairs_time
+        pred_full_time = predictions[0][1] - predictions[0][0]
+        planned_full_time = plannedTime_end - plannedTime_first
+        planned_pairs_time = plannedTime_stop - plannedTime_start
+        pred_pairs_time = pred_full_time * planned_pairs_time / planned_full_time
+
+        return pred_pairs_time
+
+    def get_lines(self):
+        '''
+	    Get available Lines from privided startID and endID
+        '''
+        startID = self.__startid
+        endID = self.__endid
+        json_file = '{}/stops.json'.format(self.__data_path)
+        all_lines = json.loads(open(json_file).read())
+        startID_lines = all_lines[str(startID)]
+        endID_lines = all_lines[str(endID)]
+        lines = set(startID_lines)&set(endID_lines)
+        new_lines = []
+        for line in lines:
+            new_lines.append(line.split('_'))
+        return new_lines
+
+    def get_all_prediction(self):
+        lines = self.get_lines()
+        if self.__debug:
+            print("Searching from bus lines table: ", lines)
+        results = []
+        for oneLine in lines:
+            results.append([oneLine[0], self.prediction(oneLine[0])])
+        return results
 
 def main():
-    print(prediction('39A', 1913, 1660, 34200))
-    print(prediction('39A', 1864, 335, 34200))
-    print(prediction('39A', 6112, 1867, 24200))
+    #my = Ann(1864,335,34200,DEBUG=True)
+    my = Ann(1913,1660,72000)
+    print(my.get_all_prediction())
+
+    #lines = get_lines(1913,1660)
+    #for oneLine in lines:
+    #    print(prediction(oneLine[0], 1913, 1660, 34200)) 
+    #print(prediction('39A', 1913, 1660, 34200))
+    #print(prediction('39A', 1864, 335, 34200))
+    #print(prediction('39A', 6112, 1867, 24200))
 
 if __name__ == '__main__':
     main()
