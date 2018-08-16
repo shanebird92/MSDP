b61468d81c4c753cc317762138f6b1cfe9d9d82a
student <peng@csstudent07.ucd.ie>
2018-08-16 12:06:18 +0100

    2018.08.16 add more keys in returned dict of transfer_input_form() of view.py

--- src/msdp/mysite/dublinbus/views.py
+++ src/msdp/mysite/dublinbus/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-import requests, json
+import requests, json, os
 
 # Create your views here.
 from django.core.cache import cache # This is the memcache cache.
@@ -58,6 +58,16 @@ def form_input(request):
 
 @csrf_exempt
 def transfer_form_input(request): 
+    data_path = os.path.dirname(os.path.abspath(__file__)) + "/../../../../data"
+    try:
+        station_json_file = '{}/stopToStationName.json'.format(data_path)
+        all_stations = json.loads(open(station_json_file).read())
+        gps_json_file = '{}/stop_latlong.json'.format(data_path)
+        all_gps = json.loads(open(gps_json_file).read())
+    except Exception as e:
+        all_stations = {}
+        all_gps = {}
+
     if request.method == 'POST':
         start = int(request.POST.get('Start'))
         end = int(request.POST.get('End'))
@@ -72,15 +82,31 @@ def transfer_form_input(request):
         solution = my.getFinalRoute()
         route = {}
         route['walk1_startID'] = solution[0]
+        route['walk1_start_station'] = all_stations[str(solution[0])].strip()
+        route['first_stop_location'] = all_gps[str(solution[0])]
+
         route['walk1_endID'] = solution[1]
+        route['walk1_end_station'] = all_stations[str(solution[1])].strip()
+
         route['walk1_distance'] = solution[2]
         route['lineA'] = solution[3][0]['firstLines']
+
         route['middle_walk_startID'] = solution[3][0]['firstLineTransferStop']
+        route['middle_walk_start_station'] = all_stations[str(solution[3][0]['firstLineTransferStop'])].strip()
+
         route['middle_walk_endID'] = solution[3][0]['secondLineTransferStop']
+        route['middle_walk_end_station'] = all_stations[str(solution[3][0]['secondLineTransferStop'])].strip()
+
         route['middle_walk_distance'] = solution[3][0]['transferDistance']
         route['lineB'] = solution[3][0]['secondLines']
+
         route['walk2_startID'] = solution[4]
+        route['walk2_start_station'] = all_stations[str(solution[4])].strip()
+
         route['walk2_endID'] = solution[5]
+        route['walk2_end_station'] = all_stations[str(solution[5])].strip()
+        route['last_stop_location'] = all_gps[str(solution[5])]
+
         route['walk2_distance'] = solution[6]
 
         json_route = json.dumps(route)
